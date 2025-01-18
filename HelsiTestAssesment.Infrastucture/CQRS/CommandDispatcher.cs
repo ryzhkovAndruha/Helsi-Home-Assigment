ab58af2e14e264341128bd62d763e833a6d4f049
ryzhkovAndruha <61603459+ryzhkovAndruha@users.noreply.github.com>
2025-01-18 15:30:41 +0200

    Implementing hanling Commands and Queries

---
+++ HelsiTestAssesment.Infrastucture/CQRS/CommandDispatcher.cs
@@ -0,0 +1,26 @@
+﻿using HelsiTestAssesment.Application.Core.Interfaces.CQRS;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace HelsiTestAssesment.Infrastucture.CQRS;
+
+public class CommandDispatcher
+{
+    private readonly IServiceProvider _serviceProvider;
+
+    public CommandDispatcher(IServiceProvider serviceProvider)
+    {
+        _serviceProvider = serviceProvider;
+    }
+
+    public async Task DispatchAsync<TCommand>(TCommand command) where TCommand : ICommand
+    {
+        var handler = _serviceProvider.GetRequiredService<ICommandHandler<TCommand>>();
+
+        if (handler == null)
+        {
+            throw new InvalidOperationException($"Handler for query {typeof(TCommand).Name} not found.");
+        }
+
+        await handler.Handle(command);
+    }
+}
---
+++ HelsiTestAssesment.Infrastucture/CQRS/QueryDispatcher.cs
@@ -0,0 +1,27 @@
+﻿using HelsiTestAssesment.Application.Core.Interfaces.CQRS;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace HelsiTestAssesment.Infrastucture.CQRS;
+
+public class QueryDispatcher
+{
+    private readonly IServiceProvider _serviceProvider;
+
+    public QueryDispatcher(IServiceProvider serviceProvider)
+    {
+        _serviceProvider = serviceProvider;
+    }
+
+    public async Task<TResult> DispatchAsync<TQuery, TResult>(TQuery query) where TQuery : IQuery<TResult>
+    {
+        var handler = _serviceProvider.GetService<IQueryHandler<TQuery, TResult>>();
+
+        if (handler == null)
+        {
+            throw new InvalidOperationException($"Handler for query {typeof(TQuery).Name} not found.");
+        }
+            
+
+        return await handler.Handle(query);
+    }
+}
